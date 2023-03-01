22837a9a08673b2fd310a6b1b3267c2992360ec6
sayan706 <sayanchatterjee706@gmail.com>
2023-03-01 17:32:42 +0530

    finalized by sayan

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
-from api.views import CompanyViewSet
+from api.views import CompanyViewSet,EmployeeViewset
 from rest_framework import routers
 
 
 router = routers.DefaultRouter()
 router.register(r'companies',CompanyViewSet)
+router.register(r'employees',EmployeeViewset)
+
 
 urlpatterns = [
     path('',include(router.urls))
